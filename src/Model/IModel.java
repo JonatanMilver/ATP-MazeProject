57747e8656d08260c2cfcade35e53c5678e4f82c
JonatanMilver <48964784+JonatanMilver@users.noreply.github.com>
2020-06-09 19:17:02 +0300

    Added front-end design - About, Help

--- src/Model/IModel.java
+++ src/Model/IModel.java
@@ -1,8 +1,11 @@
 package Model;
 
 
+import algorithms.mazeGenerators.Maze;
+
 public interface IModel {
 
-//    void generateMaze(int rows, int columns);
-//    void solveMaze(Maze maze);
+    Maze generateMaze(int rows, int columns);
+    void solveMaze(Maze maze);
+
 }
